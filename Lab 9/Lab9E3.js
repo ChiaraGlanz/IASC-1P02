af01758e3e9680c4b792ecac30a3606783b6d575
ChiaraGlanz <cg18so@brocku.ca>
2020-11-15 22:17:32 -0500

    Lab9
    Updates

---
+++ Lab 9/Lab9E3.js
@@ -0,0 +1,6 @@
+function calculation() {
+    var inputA = parseInt(document.getElementById('inputA').value);
+    var inputB = parseInt(document.getElementById('inputB').value);
+    
+    document.getElementById('output').innerHTML = inputA + inputB;
+}
